b77748da92dfb03c23a580d6dc704525d8f88c6f
Elise van Iterson <elise.van.iterson@q42.nl>
2026-04-03 10:46:37 +0200

    implement redis

--- Api/UrlEndpoints.cs
+++ Api/UrlEndpoints.cs
@@ -1,17 +1,27 @@
+using StackExchange.Redis;
+
 namespace url_shortener.Endpoints;
 
 public static class UrlEndpoints
 {
     public static void MapUrlEndpoints(this WebApplication app)
     {
-        app.MapPost("/shorten", () =>
+        app.MapPost("/shorten", async (ShortenRequest request, IConnectionMultiplexer redis) =>
         {
-            // your logic here
+            var db = redis.GetDatabase();
+            var shortCode = Guid.NewGuid().ToString()[..6];
+            await db.StringSetAsync(shortCode, request.Url);
+            return Results.Ok($"http://localhost:5292/{shortCode}");
         });
 
-        app.MapGet("/{shortCode}", () =>
+        app.MapGet("/{shortCode}", async (string shortCode, IConnectionMultiplexer redis) =>
         {
-            // your logic here
+            var db = redis.GetDatabase();
+            var url = await db.StringGetAsync(shortCode);
+            if (url.IsNullOrEmpty) return Results.NotFound();
+            return Results.Redirect(url!);
         });
     }
 }
+
+record ShortenRequest(string Url);
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using url_shortener.Endpoints;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect("localhost:6379")
+);
+
 
 var app = builder.Build();
 
